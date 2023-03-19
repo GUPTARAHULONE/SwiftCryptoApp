e58c277d434b80a3f6847dccbb912a5854bf1fd9
RahulGupta <rahul.gu@peopleinteractive.in>
2023-03-19 23:20:04 +0530

    Added UI for Coin Row Element  and extension function for formatter of Double

---
+++ SwiftCryptoApp/core/Home/Views/CoinRowView.swift
@@ -0,0 +1,86 @@
+//
+//  CoinRowView.swift
+//  SwiftCryptoApp
+//
+//  Created by Rahul Gupta on 19/03/23.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    
+    let coin : CoinModel
+    let showHoldingColumn : Bool
+    var body: some View {
+        HStack(spacing: 0) {
+            leftColumn
+            Spacer()
+            
+            if showHoldingColumn {
+               centreColumn
+            }
+            
+            rightColumn
+        }
+        .font(.subheadline)
+    }
+}
+
+struct CoinRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            CoinRowView(coin: dev.coin,showHoldingColumn: true)
+                .previewLayout(.sizeThatFits)
+            
+            CoinRowView(coin: dev.coin,showHoldingColumn: true)
+                .previewLayout(.sizeThatFits)
+                .preferredColorScheme(.dark)
+
+        }
+    }
+}
+
+
+extension CoinRowView {
+    
+    private var leftColumn : some View {
+        HStack(spacing: 0) {
+            //making int to string using backslash
+            Text("\(coin.rank)")
+                .font(.caption)
+                .foregroundColor(Color.theme.secondaryText)
+                .frame(minWidth: 30)
+            Circle()
+                .frame(width: 30, height: 30)
+            Text(coin.symbol.uppercased())
+                .font(.headline)
+                .padding(.leading,6)
+                .foregroundColor(Color.theme.accent)
+        }
+    }
+    
+    private var centreColumn : some View {
+        VStack(alignment: .trailing) {
+            Text(coin.currentHoldingsValue.asCurrencyWith2Decimals())
+                .bold()
+            Text((coin.currentHoldings ?? 0).asNumberString())
+        }
+        .foregroundColor(Color.theme.accent)
+    }
+    
+    private var rightColumn : some View {
+        VStack(alignment: .trailing) {
+            Text(coin.currentPrice.asCurrencyWith6Decimals())
+                .bold()
+                .foregroundColor(Color.theme.accent)
+            Text(coin.priceChangePercentage24H?.asPercentString() ?? "")
+                .foregroundColor(
+                    (coin.priceChangePercentage24H ?? 0) >= 0
+                    ? Color.theme.green
+                    : Color.theme.red
+                )
+        }
+        .frame(width: UIScreen.main.bounds.width/3.5 , alignment: .trailing)
+    }
+
+}
