0cdc67b2826cc38435f1c60a608f3e0b65a65adb
RahulGupta <rahul.gu@peopleinteractive.in>
2023-03-25 12:46:33 +0530

    integrate search list of coins based the input of search items .

--- SwiftCryptoApp/core/Home/ViewModel/HomeViewModel.swift
+++ SwiftCryptoApp/core/Home/ViewModel/HomeViewModel.swift
@@ -25,12 +25,41 @@ class HomeViewModel : ObservableObject {
     }
     
     private func addSubscribers() {
-        dataService.$allCoins
+        
+        // we are not using this becoz, this service used in the search text ,so that
+        // it get subscribing from there no need to again subscribes.
+//        dataService.$allCoins
+//            .sink { [weak self] (returnedCoins) in
+//                self?.allCoins = returnedCoins
+//
+//            }
+//            .store(in: &cancellables)
+//
+        // debounce is used ofr waiting 0.5 sec before calling filter coins
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map(filterCoins)  // as map is providing both values based on two publisher  so need pass values in filtercoins fxn.
             .sink { [weak self] (returnedCoins) in
-                self?.allCoins = returnedCoins
-                
-            }
-            .store(in: &cancellables)
+                            self?.allCoins = returnedCoins
+            
+                }
+                .store(in: &cancellables)
+    }
+    
+    private func filterCoins(text: String , coins: [CoinModel]) -> [CoinModel] {
+        
+        guard !text.isEmpty else {
+            return coins
+        }
+        
+        let lowercasedText = text.lowercased()
+        
+        return coins.filter { (coin) -> Bool in
+            return coin.name.lowercased().contains(lowercasedText) ||
+            coin.symbol.lowercased().contains(lowercasedText) ||
+            coin.id.lowercased().contains(lowercasedText)
+        }
     }
 
 }
